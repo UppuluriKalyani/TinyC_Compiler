06604398f1a22ae96c89b08ffbd55a653a3dc4b6
Kalyani Uppuluri <105410881+UppuluriKalyani@users.noreply.github.com>
2024-12-29 08:58:53 +0530

    Create L2Lexer.py

---
+++ TinyC Level1/Target Code Generator/L2Lexer.py
@@ -0,0 +1,47 @@
+from sly import Lexer
+
+class LexicalAnalyzer(Lexer):
+    literals = {'(', ')', '{', '}', ';', ',', '=', '+', '-', '*', '/', '%', '<', '>'}
+    tokens = {
+        NUMBER, FLOAT, ID, INT, DOUBLE, PRINT, EQUAL, NOTEQUAL,
+        GREATEREQUAL, LESSEQUAL, LOGICALAND, LOGICALOR
+    }
+    ignore = ' \t'
+
+    # Regular expressions for tokens
+    FLOAT = r'[0-9]+\.[0-9]+'
+    NUMBER = r'[0-9]+'
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+    # Keywords
+    ID['int'] = INT
+    ID['double'] = DOUBLE
+    ID['print'] = PRINT
+
+    # Operators
+    GREATEREQUAL = r'>='
+    LESSEQUAL = r'<='
+    EQUAL = r'=='
+    NOTEQUAL = r'!='
+    LOGICALAND = r'&&'
+    LOGICALOR = r'\|\|'
+
+    # Ignored patterns
+    ignore_comment = r'\#.*'
+
+    # Handlers
+    def FLOAT(self, t):
+        t.value = float(t.value)
+        return t
+
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print(f"Illegal character {t.value[0]} on line {self.lineno} {self.index}")
+        self.index += 1
